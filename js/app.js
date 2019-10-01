8e473d199dc4f3835e21be69b701f10a39301fb2
Jonathan Huynh <jonhuynh05@gmail.com>
2019-10-01 15:49:04 -0700

    add buttons to screen

--- js/app.js
+++ js/app.js
@@ -11,9 +11,9 @@ class Tomagotchi {
 // const name = prompt("Name your pet")
 const firstTomagotchi = new Tomagotchi(name);
 
-const $div = $("div");
+const $divStatus = $(".status");
 const tomagotchiArray = Object.values(firstTomagotchi);
 
 for (let i = 1; i < 5; i++){
-   $div[i-1].append(tomagotchiArray[i]);
+   $divStatus[i-1].append(tomagotchiArray[i]);
 };
